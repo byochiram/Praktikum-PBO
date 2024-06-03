75f820d9a3bf0e8aef1ebe9036979dbd9a930a14
byochiram <149850348+byochiram@users.noreply.github.com>
2024-06-03 23:31:41 +0700

    Delete Kryptonian.java

--- Kryptonian.java
+++
@@ -1,9 +0,0 @@
-/*
- * Nama      : Rosidah Rahmati
- * NIM       : 24060122140121
- * File      : Kryptonian.java
- * Deskripsi : Abstract Class Kryptonian r
- */
-
-public abstract class Kryptonian extends Animal implements IFlyer{
-}
