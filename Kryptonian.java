e7a6ad44551aae04c3884f31791e4022f3c1d5a3
byochiram <149850348+byochiram@users.noreply.github.com>
2024-06-03 23:38:58 +0700

    Add files via upload

---
+++ Kryptonian.java
@@ -0,0 +1,9 @@
+/*
+ * Nama      : Rosidah Rahmati
+ * NIM       : 24060122140121
+ * File      : Kryptonian.java
+ * Deskripsi : Abstract Class Kryptonian r
+ */
+
+public abstract class Kryptonian extends Animal implements IFlyer{
+}
