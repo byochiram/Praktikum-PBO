289c9f4b45bd9dac83831f1572ec7d9d30880600
byochiram <149850348+byochiram@users.noreply.github.com>
2024-06-03 23:29:21 +0700

    Delete Airplane.java

--- Airplane.java
+++
@@ -1,9 +0,0 @@
-/*
- * Nama      : Rosidah Rahmati
- * NIM       : 24060122140121
- * File      : Airplane.java
- * Deskripsi : Abstract class Airplane
- */
-
-public abstract class Airplane extends Vehicle implements IFlyer{
-}
